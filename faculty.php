ed7f66abd415d4c51a16be75263d3c17bdf38c67
SejalJain-07 <sejal99jain.sj@gmail.com>
2021-02-07 20:38:23 +0530

    updated footer, completed faculty page and basic css modification

--- faculty.php
+++ faculty.php
@@ -23,18 +23,58 @@
 				<div class="container">
 					<h3 class="w3l_header w3_agileits_header">About Our <span>Faculty</span></h3>
 					<div class="wthree_services_grids">	
+						
+						<!--Prof. Hitesh Bechra-->
 						<div class="col-md-6 wthree_services_grid_right">
-							<img src="images/HiteshBechra-Physics.png" alt="" height="350px">
-							<div class="clearfix"> </div>
+							<img src="images/HiteshBechra-Physics.png" alt="" height="260px">
+
 						</div>
 						<div class="col-md-6 wthree_services_grid_left">
-							<h3 class='text-left'><br>Prof. <span>Hitest Becha</span></h3>
+							<h3 class='text-left'><br>Prof. <span>Hitesh Bechra</span></h3>
 							<h4>PHYSICS</h4>
 							<p>Prof. Hitesh Bechra worked in a multinational company for a short span after completing his Engineering in Electronics domain. His passion towards teaching brought him back in the education domain. He has worked in many renowned coaching institutes before starting TSPH. Connecting the Concept of Physics with the real world and rich subject knowledge are some of the characteristics which represent his personality. 
 							</p>
 						</div>
-						
 						<div class="clearfix"> </div>
+						
+						<!--Prof. Parimal Patel-->
+						<div class="col-md-6 wthree_services_grid_right">
+							<img src="images/ParimalPatel-Chemistry.png" alt="" height="270px">
+						</div>
+						<div class="col-md-6 wthree_services_grid_left">
+							<h3 class='text-left'><br>Prof. <span>Parimal Patel</span></h3>
+							<h4>CHEMISTRY</h4>
+							<p>Prof. Parimal Patel has completed B.Pharm from Institute of Chemical Technology (ICT). Stronghold on the subject and friendly environment in the classroom makes students fall in love with Chemistry. Along with Scientific knowledge, sir strongly believes in imparting spiritual knowledge also. Yoga sessions and lesson on physical exercise from Parimal sir helps students in better concentration in studies and learning. Sir is also certified yoga teacher from Ayush Mantralaya, Governmet of India. 
+							</p>
+						</div>
+						<div class="clearfix"> </div>
+						
+						<!--Prof. Pranav Gandhi-->
+						<div class="col-md-6 wthree_services_grid_right">
+							<img src="images/PranavGandhi-Maths.png" alt="" height="260px">
+							<div class="clearfix"> </div>
+						</div>
+						<div class="col-md-6 wthree_services_grid_left">
+							<h3 class='text-left'><br>Prof. <span>Pranav Gandhi</span></h3>
+							<h4>Mathematics</h4>
+							<p>Have you heard about musical Mathematics? Prof. Pranav Gandhi’s classroom is the perfect example of delivering Mathematics in musical format. Academics wise sir is coming from a management background. In the past two decades, sir has transformed the lives of many youngsters. Students from sir’s early teaching days have now become renowned Engineer and Doctor and serving across the Globe. Career counseling and motivational session of sir keeps students on their toe across the two years of academic program. 
+							</p>
+						</div>
+						<div class="clearfix"> </div>
+						
+						<!--Prof. Gyanprakash Pandey-->
+						<div class="col-md-6 wthree_services_grid_right">
+							<img src="images/GyanprakashPandey-Biology.png" alt="" height="300px">
+							<div class="clearfix"> </div>
+						</div>
+						<div class="col-md-6 wthree_services_grid_left">
+							<h3 class='text-left'><br>Prof. <span>Gyanprakash Pandey</span></h3>
+							<h4>Biology</h4>
+							<p>To become a doctor; one must learn from the one who himself is a doctor. Since the past 15 years, Prof. Dr. Gyanprakash Pandey sir has guided thousands of medical aspirants. Biology subject requires practical demonstration and to the point explanation to understand the core concepts thoroughly. Sir understands that and hence use of specimen as well as technology-aided teaching makes his classroom very much lively and interesting. His academic background enables him to give proper guidance to young medical aspirants. 
+							</p>
+						</div>
+						<div class="clearfix"> </div>
+						
 					</div>
 				</div>
 			</div>
--- include/footer.php
+++ include/footer.php
@@ -2,31 +2,44 @@
 <div class="footer">
 	<div class="f-bg-w3l">
 		<div class="container">
-			<div class="col-md-4 w3layouts_footer_grid">
+			<div class="col-md-3 w3layouts_footer_grid">
 				<h2>Follow <span>Us</span></h2>
 				<ul class="social_agileinfo">
-					<li><a href="#" class="w3_facebook"><i class="fa fa-facebook"></i></a></li>
-					<li><a href="#" class="w3_twitter"><i class="fa fa-twitter"></i></a></li>
-					<li><a href="#" class="w3_instagram"><i class="fa fa-instagram"></i></a></li>
-					<li><a href="#" class="w3_google"><i class="fa fa-google-plus"></i></a></li>
+					<li><a href="https://www.facebook.com/tsphofficial/" class="w3_facebook"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="https://www.youtube.com/channel/UC_hBrnDRNGlle41c-E9uPFw" class="w3_youtube"><i class="fa fa-youtube-play"></i></a></li>
+					<li><a href="https://www.instagram.com/tsph_thescienceprivateshub/" class="w3_instagram"><i class="fa fa-instagram"></i></a></li>
+					<li><a href="http://tsph.classmatrix.in/" class="w3_google"><i class="fa fa-google-plus"></i></a></li>
 				</ul>
 			</div>
-			<div class="col-md-8 w3layouts_footer_grid">
-				<form action="#" method="post">
-					<input type="email" name="Email" placeholder="Email..." required="">
-					<input type="submit" value="">
-				</form>
+			<div class="col-md-9 w3layouts_footer_grid">
 				<ul class="w3l_footer_nav">
-					<li><a href="index.html" class="active">Home</a></li>
-					<li><a href="services.html">Faculty</a></li>
-					<li><a href="gallery.html">Courses</a></li>
-					<li><a href="icons.html">Results</a></li>
-					<li><a href="mail.html">Gallery</a></li>
-					<li><a href="mail.html">Contact Us</a></li>
+					<li><a href="index.php" class="active">Home</a></li>
+					<li><a href="faculty.php">Faculty</a></li>
+					<li><a href="courses.php">Courses</a></li>
+					<li><a href="results.php">Results</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contactus.php">Contact Us</a></li>
 				</ul>
-				<p>© 2017 Mastering. All Rights Reserved.</p>
+			</div>
+			<div class="col-md-12 w3layouts_footer_grid">
+				<h2><span>Branch</span> Address</h2>
+				<div class="col-md-6 w3layouts_footer_grid nopadding">
+					<p><b>BHAYANDAR BRANCH</b></p>
+					<p>First Floor, Ashoka Shopping Centre, Navghar Road,Near Shani Mandir, Near Railway Station, Bhayandar (E),</p>
+					<a class="agile-icon" href="#" style="color:white;"> <i class=" fa-map-marker"></i></a>
+					<a href="tel:+918652375375" style="font-size:15px; color:white;" onMouseOver="this.style.color='cyan'"onMouseOut="this.style.color='#fff'">8652 375 375</a><a style="color:white;"> |</a>
+					<a href="tel:+918652374374" style="font-size:15px; color:white;" onMouseOver="this.style.color='cyan'"onMouseOut="this.style.color='#fff'">8652 374 374</a>
+				</div>
+				<div class="col-md-6 w3layouts_footer_grid">
+					<p><b>MIRAROAD BRANCH</b></p> 
+					<p>D/232, First Floor, Shanti Shopping Centre, Near Railway Station, Mira Road (E),</p>
+					<a class="agile-icon" href="#" style="color:white;"> <i class=" fa-map-marker"></i></a>
+					<a href="tel:+918652371371" style="font-size:15px; color:white;" onMouseOver="this.style.color='cyan'"onMouseOut="this.style.color='#fff'">8652 371 371</a><a style="color:white;"> |</a>
+					<a href="tel:+918652373373" style="font-size:15px; color:white;" onMouseOver="this.style.color='cyan'"onMouseOut="this.style.color='#fff'">8652 373 373</a>
+				</div>
 			</div>
 			<div class="clearfix"> </div>
 		</div>
+		<p class="text-center" style="color:white;">© 2021 Mastering. All Rights Reserved.</p>
 	</div>
 </div>
