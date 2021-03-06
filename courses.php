35690092cf8ecfa50ef21f04fa9fdd4356a31737
SejalJain-07 <sejal99jain.sj@gmail.com>
2021-03-06 23:34:02 +0530

    Created a rough template of courses page

---
+++ courses.php
@@ -0,0 +1,189 @@
+<?php include "include/security.php" ?>
+<?php $_SESSION['pageName'] = 'courses' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "include/title.php"; ?>
+	<?php include "include/css.php"; ?>
+</head>
+	
+<body>
+	<?php include "include/topNav.php"; ?>
+	
+	<div class="banner1">
+			
+		<div class="w3_agileits_service_banner_info">
+			<h2>Courses</h2>
+		</div>
+	</div>
+<!-- //banner -->
+<!-- services -->
+	<div class="services two">
+		<div class="container">
+			<h3 class="w3l_header w3_agileits_header">Engineering  <span>Programs</span></h3>
+			<div class="wthree_services_grids">
+				<div class="col-md-12 wthree_services_grid_right">
+					<div class="col-md-4 agileits_w3layouts_service_grid">
+						<div class="agile_service_grd">
+							<i class="fa-stack" aria-hidden="true">
+								<strong class="fa-stack-1x">
+									1
+								</strong>
+							</i>
+						</div>
+						<h4>Two Year Classroom Program</h4>
+						<p>Tortor varius augue ut mauris dictum.</p>
+					</div>
+					<div class="col-md-4 agileits_w3layouts_service_grid">
+						<div class="agile_service_grd">
+						<i class="fa-stack" aria-hidden="true">
+							<strong class="fa-stack-1x">
+								2
+							</strong>
+						</i>
+						</div>
+						<h4>One Year Classroom Program</h4>
+						<p>Tortor varius augue ut mauris dictum.</p>
+					</div>
+					<div class="col-md-4 agileits_w3layouts_service_grid">
+						<div class="agile_service_grd">
+						<i class="fa-stack" aria-hidden="true">
+							<strong class="fa-stack-1x">
+								3
+							</strong>
+						</i>
+						</div>
+						<h4>Special Repeaters Batch Program</h4>
+						<p>Tortor varius augue ut mauris dictum.</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="col-md-12 col-sm-12">
+				<div style="height:80px;"></div>
+			</div>
+			<h3 class="w3l_header w3_agileits_header">Medical  <span>Programs</span></h3>
+			<div class="wthree_services_grids">
+				<div class="col-md-12 wthree_services_grid_right">
+					<div class="col-md-4 agileits_w3layouts_service_grid">
+						<div class="agile_service_grd">
+							<i class="fa-stack" aria-hidden="true">
+								<strong class="fa-stack-1x">
+									1
+								</strong>
+							</i>
+						</div>
+						<h4>Two Year Classroom Program</h4>
+						<p>Tortor varius augue ut mauris dictum.</p>	
+					</div>
+					<div class="col-md-4 agileits_w3layouts_service_grid">
+						<div class="agile_service_grd">
+						<i class="fa-stack" aria-hidden="true">
+							<strong class="fa-stack-1x">
+								2
+							</strong>
+						</i>
+						</div>
+						<h4>One Year Classroom Program</h4>
+						<p>Tortor varius augue ut mauris dictum.</p>
+					</div>
+					<div class="col-md-4 agileits_w3layouts_service_grid">
+						<div class="agile_service_grd">
+						<i class="fa-stack" aria-hidden="true">
+							<strong class="fa-stack-1x">
+								3
+							</strong>
+						</i>
+						</div>
+						<h4>Special Repeaters Batch Program</h4>
+						<p>Tortor varius augue ut mauris dictum.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!-- //services -->
+<!-- stats -->
+	<div class="stats" id="stats">
+	    <div class="container"> 
+			<div class="inner_w3l_agile_grids">
+		<div class="col-md-3 w3layouts_stats_left w3_counter_grid">
+		   <i class="fa fa-laptop" aria-hidden="true"></i>
+			<p class="counter">45</p>
+			<h3>Courses</h3>
+		</div>
+		<div class="col-md-3 w3layouts_stats_left w3_counter_grid1">
+		    <i class="fa fa-smile-o" aria-hidden="true"></i>
+			<p class="counter">165</p>
+			<h3>Members</h3>
+		</div>
+		<div class="col-md-3 w3layouts_stats_left w3_counter_grid2">
+		<i class="fa fa-trophy" aria-hidden="true"></i>
+			<p class="counter">563</p>
+			<h3>Awards</h3>
+		</div>
+		<div class="col-md-3 w3layouts_stats_left w3_counter_grid3">
+		<i class="fa fa-user" aria-hidden="true"></i>
+			<p class="counter">245</p>
+			<h3>Instructors</h3>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+   </div>	
+</div>
+<!-- //stats -->
+	<div class="col-md-12 col-sm-12">
+		<div style="height:80px;"></div>
+	</div>
+<h3 class="w3l_header w3_agileits_header">Medical  <span>Programs</span></h3>
+			<div class="wthree_services_grids">
+				<div class="col-md-12 wthree_services_grid_right">
+					<div class="col-md-4 agileits_w3layouts_service_grid">
+						<div class="agile_service_grd">
+							<i class="fa-stack" aria-hidden="true">
+								<strong class="fa-stack-1x">
+									1
+								</strong>
+							</i>
+						</div>
+						<h4>Two Year Classroom Program</h4>
+						<p>Tortor varius augue ut mauris dictum.</p>	
+					</div>
+					<div class="col-md-4 agileits_w3layouts_service_grid">
+						<div class="agile_service_grd">
+						<i class="fa-stack" aria-hidden="true">
+							<strong class="fa-stack-1x">
+								2
+							</strong>
+						</i>
+						</div>
+						<h4>One Year Classroom Program</h4>
+						<p>Tortor varius augue ut mauris dictum.</p>
+					</div>
+					<div class="col-md-4 agileits_w3layouts_service_grid">
+						<div class="agile_service_grd">
+						<i class="fa-stack" aria-hidden="true">
+							<strong class="fa-stack-1x">
+								3
+							</strong>
+						</i>
+						</div>
+						<h4>Special Repeaters Batch Program</h4>
+						<p>Tortor varius augue ut mauris dictum.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-12 col-sm-12">
+		<div style="height:150px;"></div>
+	</div>
+
+	<?php include "include/footer.php"; ?>
+	<?php include "include/js.php"; ?>
+</body>
+</html>
