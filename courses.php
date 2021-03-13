8096646ee1d80f9a1eca5465d8075e96610d883a
SejalJain-07 <sejal99jain.sj@gmail.com>
2021-03-13 22:06:42 +0530

    Updated courses page with details

--- courses.php
+++ courses.php
@@ -20,10 +20,11 @@
 <!-- services -->
 	<div class="services two">
 		<div class="container">
+			<!-- Engineering Programs -->
 			<h3 class="w3l_header w3_agileits_header">Engineering  <span>Programs</span></h3>
 			<div class="wthree_services_grids">
-				<div class="col-md-12 wthree_services_grid_right">
-					<div class="col-md-4 agileits_w3layouts_service_grid">
+				<div class="col-md-6 wthree_services_grid_right">
+					<div class="col-md-3 agileits_w3layouts_service_grid">
 						<div class="agile_service_grd">
 							<i class="fa-stack" aria-hidden="true">
 								<strong class="fa-stack-1x">
@@ -32,9 +33,8 @@
 							</i>
 						</div>
 						<h4>Two Year Classroom Program</h4>
-						<p>Tortor varius augue ut mauris dictum.</p>
 					</div>
-					<div class="col-md-4 agileits_w3layouts_service_grid">
+					<div class="col-md-3 agileits_w3layouts_service_grid">
 						<div class="agile_service_grd">
 						<i class="fa-stack" aria-hidden="true">
 							<strong class="fa-stack-1x">
@@ -43,9 +43,8 @@
 						</i>
 						</div>
 						<h4>One Year Classroom Program</h4>
-						<p>Tortor varius augue ut mauris dictum.</p>
 					</div>
-					<div class="col-md-4 agileits_w3layouts_service_grid">
+					<div class="col-md-3 agileits_w3layouts_service_grid">
 						<div class="agile_service_grd">
 						<i class="fa-stack" aria-hidden="true">
 							<strong class="fa-stack-1x">
@@ -54,18 +53,55 @@
 						</i>
 						</div>
 						<h4>Special Repeaters Batch Program</h4>
-						<p>Tortor varius augue ut mauris dictum.</p>
+					</div>
+				</div>
+				<!-- Engg - Two Year Classroom Program -->
+				<div class="col-md-6 wthree_services_grid_left">
+					<div class="col-md-6 wthree_services_grid_right">
+						<h3 class='text-left nomargin' style='font-size:1.5em'>1.JEE Advanced 2 Years Course  <span>(XI + XII + JEE Mains & Advanced + MHCET)</span></h3>
+						<div style='height:15px;'></div>
+						<p>This is a perfect program for those aspiring Engineers who want to pursue engineering from Indian Institute of Technology (IIT) or Indian Institute of Science (IISc). The course is designed in such a way that along with the preparation of JEE Advanced, students will be well versed with the preparation of Boards and MHCET. The program commences in May month.</p>
+					</div>
+					<div class="col-md-6 wthree_services_grid_left">
+						<h3 class='text-left nomargin' style='font-size:1.5em'>2.JEE Mains 2 Years Course  <span>(XI + XII + JEE Mains + MHCET)</span></h3>
+						<div style='height:15px;'></div>
+						<p>This is a perfect program for those aspiring Engineers who want to pursue engineering from either National Institute of Technology (NIT) or Indian Institute of Information Technology (IIIT) or Central Government Funded Technology Institutes (GFTI) or State Government Funded Colleges. This program balances preparations of boards as well as entrance examination. The course is designed in such a way that students are thoroughly prepared for JEE Mains and they will get an upper edge in MHCET examination. The program commences in May month.</p>
+					</div>
+				</div>
+				
+				<!-- Engg - One Year Classroom Program -->
+				<div class="col-md-6 wthree_services_grid_left">
+					<div class="col-md-6 wthree_services_grid_right">
+						<h3 class='text-left nomargin' style='font-size:1.5em'>1.JEE Mains 1Year Course <span>(XII + JEE Mains + MHCET)</span></h3>
+						<div style='height:15px;'></div>
+						<p>The program covers the preparation of HSC boards, JEE Mains and MHCET entrance examination. Entrance preparation will be on the fast track to ensure that students are prepared with the entire syllabus. The program commences in December month (when the student is in STD. XI).</p>
+					</div>
+				</div>
+				
+				<!-- Engg - Special Repeaters Batch Program -->
+				<div class="col-md-6 wthree_services_grid_left">
+					<div class="col-md-6 wthree_services_grid_right">
+						<h3 class='text-left nomargin' style='font-size:1.5em'>1.JEE Mains <span>Repeater</span></h3>
+						<div style='height:15px;'></div>
+						<p>Special JEE Mains repeaters course is designed for those who missed NIT by a small margin in the first attempt; why don’t give one more chance? Systemic revision</p>
+					</div>
+					<div class="col-md-6 wthree_services_grid_left">
+						<h3 class='text-left nomargin' style='font-size:1.5em'>2.JEE Advanced <span>Repeater </span></h3>
+						<div style='height:15px;'></div>
+						<p>Special JEE Advanced repeater is designed for those who missed IIT by a small margin in the first attempt; why don’t give one more chance? Systemic revision, doubts solving, part tests and full syllabus mock tests will ensure that student will definitely achieve the target.</p>
 					</div>
 				</div>
 				<div class="clearfix"> </div>
 			</div>
+			
 			<div class="col-md-12 col-sm-12">
 				<div style="height:80px;"></div>
 			</div>
+			<!-- Medical Programs -->
 			<h3 class="w3l_header w3_agileits_header">Medical  <span>Programs</span></h3>
 			<div class="wthree_services_grids">
-				<div class="col-md-12 wthree_services_grid_right">
-					<div class="col-md-4 agileits_w3layouts_service_grid">
+				<div class="col-md-6 wthree_services_grid_right">
+					<div class="col-md-3 agileits_w3layouts_service_grid">
 						<div class="agile_service_grd">
 							<i class="fa-stack" aria-hidden="true">
 								<strong class="fa-stack-1x">
@@ -73,10 +109,9 @@
 								</strong>
 							</i>
 						</div>
-						<h4>Two Year Classroom Program</h4>
-						<p>Tortor varius augue ut mauris dictum.</p>	
+						<h4>Two Year Classroom Program</h4>	
 					</div>
-					<div class="col-md-4 agileits_w3layouts_service_grid">
+					<div class="col-md-3 agileits_w3layouts_service_grid">
 						<div class="agile_service_grd">
 						<i class="fa-stack" aria-hidden="true">
 							<strong class="fa-stack-1x">
@@ -85,9 +120,8 @@
 						</i>
 						</div>
 						<h4>One Year Classroom Program</h4>
-						<p>Tortor varius augue ut mauris dictum.</p>
 					</div>
-					<div class="col-md-4 agileits_w3layouts_service_grid">
+					<div class="col-md-3 agileits_w3layouts_service_grid">
 						<div class="agile_service_grd">
 						<i class="fa-stack" aria-hidden="true">
 							<strong class="fa-stack-1x">
@@ -96,8 +130,30 @@
 						</i>
 						</div>
 						<h4>Special Repeaters Batch Program</h4>
-						<p>Tortor varius augue ut mauris dictum.</p>
 					</div>
+				</div>
+					
+					<!-- Med - Two Year Classroom Program -->
+				<div class="col-md-6 wthree_services_grid_left">
+						<h3 class='text-left nomargin' style='font-size:1.5em'>1.NEET 2 Years Course <span>(XI + XII + NEET + MHCET)</span></h3>
+						<div style='height:15px;'></div>
+						<p>This is a perfect program for those aspiring Doctors who want to pursue career in medical domain. This program balances the preparation of boards as well as entrance. The course is designed in such a way that students are thoroughly prepared for NEET. MHCET is for those candidates who want to pursue Pharmacy. The program commences in May month.</p>
+				</div>
+				
+				<!-- Med - One Year Classroom Program -->
+				<div class="col-md-6 wthree_services_grid_left">
+						<h3 class='text-left nomargin' style='font-size:1.5em'>1.NEET 1 Year Course <span>(XII + NEET + MHCET)</span></h3>
+						<div style='height:15px;'></div>
+						<p>Program covers the preparation of HSC boards, NEET and MHCET entrance examination. Entrance preparation will be on the fast track to ensure students are prepared with the entire syllabus. The program commences in December month (when the student is in STD. XI).</p>
+				</div>
+				
+				<!-- Med - Special Repeaters Batch Program -->
+				<div class="col-md-6 wthree_services_grid_left">
+						<h3 class='text-left nomargin' style='font-size:1.5em'>1.NEET <span>Repeaters</span></h3>
+						<div style='height:15px;'></div>
+						<p>Special NEET Repeaters course is designed for those who missed government MBBS or BDS seat by a small margin in the first attempt; why don’t give one more chance? Systemic revision, doubts solving, part tests and full syllabus mock tests will ensure that student will definitely achieve the Target.</p>
+				</div>
+					
 					<div class="clearfix"> </div>
 				</div>
 				<div class="clearfix"> </div>
