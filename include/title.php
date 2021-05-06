b522fb62c3c1ab859e1cea73960a24066e228582
sunil007 <sunil.kothari.007@gmail.com>
2021-05-06 23:21:41 +0530

    test

--- include/title.php
+++ include/title.php
@@ -1,7 +1,7 @@
 <title>The Science Private's Hub</title>
 <!-- custom-theme -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <meta name="keywords" content="Mastering Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <link rel="icon" href="images/fav.png" type="image/png" sizes="16x16">
