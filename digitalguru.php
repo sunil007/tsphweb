e8ba92b2d091be69f4d842202ea4518a99e5243f
SejalJain-07 <sejal99jain.sj@gmail.com>
2021-05-16 14:45:43 +0530

    Roughly created tsph digital guru page

--- digitalguru.php
+++ digitalguru.php
@@ -10,17 +10,285 @@
 <body>
 	<?php include "include/topNav.php"; ?>
 	
+	
 	<div class="banner1">			
 		<div class="w3_agileits_service_banner_info">
 			<h2>TSPH Digital Guru</h2>
 		</div>
 	</div>
+
+	<!--div class="col-md-12">
+		<div style="height:50px;"></div>
+	</div>
+	<div class="clearfix"></div-->
 	
+		<!-- banner -->
+	<div class="banner-silder">
+		<div id="JiSlider" class="jislider">
+			<ul>
+				<li>
+					<div class="w3layouts-banner-top" style='background-image: url(images/banner/banner1.jpg);'>
+						<div class="container">
+							<div class="agileits-banner-info">
+							</div>	
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="w3layouts-banner-top" style='background-image: url(images/banner/banner2.jpg);'>
+						<div class="container">
+							<div class="agileits-banner-info">
+							</div>	
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="w3layouts-banner-top" style='background-image: url(images/banner/banner3.jpg);'>
+						<div class="container">
+							<div class="agileits-banner-info">
+							</div>	
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="w3layouts-banner-top" style='background-image: url(images/banner/banner4.jpg);'>
+						<div class="container">
+							<div class="agileits-banner-info">
+							</div>	
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="w3layouts-banner-top" style='background-image: url(images/banner/banner5.jpg);'>
+						<div class="container">
+							<div class="agileits-banner-info">
+							</div>	
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+<!-- //banner -->
 
 	<div class="col-md-12">
 		<div style="height:50px;"></div>
 	</div>
 	<div class="clearfix"></div>
+	
+<!-- services -->
+	<div class="services two">
+		<div class="container">
+			<!-- TSPH Digital Guru -->
+			<!--h3 class="w3l_header w3_agileits_header">TSPH <span>Engineering Programs</span></h3-->
+			<div style='height:30px'></div>
+			
+			<!-- Accordian -->
+			<div class="accordion" id="accordionExample">
+				<!-- CLASS ON CLICK -->
+				<div class="card">
+					<div class="card-header" id="headingOne">
+						<h2 class="mb-0 nofocus">
+							<button class="nofocus btn btn-link fullExpand text-left collapsed" type="button" data-toggle="collapse" data-target="#engcollapseOne">
+								<h4 class='accordHeader'>
+									<div class='row nomargin'>
+										<div class='col-xs-11 nopadding' style='overflow: hidden;text-overflow: ellipsis;'><i class="fa fa-play"></i> CLASS ON CLICK </div>
+										<div class='col-xs-1 nopadding'>
+											<div style="float: right;">
+												<i class="fa fa-arrow-right"></i>
+												<i class="fa fa-arrow-down"></i>
+											</div>
+										</div>
+									</div>
+								</h4>
+							</button>
+						</h2>
+					</div>
+
+					<div id="engcollapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+						<div class="card-body accordBody wthree_services_grid_left">
+							<div style='height:10px'></div>
+							<!--h3 class="text-left nomargin" style="font-size:1.5em">1.JEE Advanced 2 Years Course  <span>(XI + XII + JEE Mains &amp; Advanced + MHCET)</span></h3>
+							<div style='height:10px'></div-->
+							<p>Join the classroom from anywhere. Anytime access to pre-recorded videos of the entire JEE, NEET Syllabus. You can revise and learn at your pace.</p>
+							<!--div style='height:30px'></div-->
+							<div style='height:5px'></div>
+						</div>
+					</div>
+				</div>
+			  
+				<!-- TEACHER ON TIP -->
+				<div class="card">
+					<div class="card-header" id="headingOne">
+						<h2 class="mb-0 nofocus">
+							<button class="nofocus btn btn-link fullExpand text-left collapsed" type="button" data-toggle="collapse" data-target="#engcollapseTwo">
+								<h4 class='accordHeader'>
+									<div class='row nomargin'>
+										<div class='col-xs-11 nopadding' style='overflow: hidden;text-overflow: ellipsis;'><i class="fa fa-play"></i> TEACHER ON TIP </div>
+										<div class='col-xs-1 nopadding'>
+											<div style="float: right;">
+												<i class="fa fa-arrow-right"></i>
+												<i class="fa fa-arrow-down"></i>
+											</div>
+										</div>
+									</div>
+								</h4>
+							</button>
+						</h2>
+					</div>
+
+					<div id="engcollapseTwo" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+						<div class="card-body accordBody wthree_services_grid_left">
+							<div style='height:10px'></div>
+							<!--h3 class="text-left nomargin" style="font-size:1.5em">1.JEE Mains 1Year Course <span>(XII + JEE Mains + MHCET)</span></h3>
+							<div style='height:10px'></div-->
+							<p>Video solutions of MCQs recorded by teachers, will be just a click away. It will greatly help students to learn from their mistakes and improve in upcoming tests. Doubts solving is a key to crack the JEE & NEET entrance exam.</p>
+							<div style='height:5px'></div>
+						</div>
+					</div>
+				</div>
+			  
+				<!-- CBT -->
+				<div class="card">
+					<div class="card-header" id="headingOne">
+						<h2 class="mb-0 nofocus">
+							<button class="nofocus btn btn-link fullExpand text-left collapsed" type="button" data-toggle="collapse" data-target="#engcollapseThree">
+								<h4 class='accordHeader'>
+									<div class='row nomargin'>
+										<div class='col-xs-11 nopadding' style='overflow: hidden;text-overflow: ellipsis;'><i class="fa fa-play"></i> CBT </div>
+										<div class='col-xs-1 nopadding'>
+											<div style="float: right;">
+												<i class="fa fa-arrow-right"></i>
+												<i class="fa fa-arrow-down"></i>
+											</div>
+										</div>
+									</div>
+								</h4>
+							</button>
+						</h2>
+					</div>
+
+					<div id="engcollapseThree" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+						<div class="card-body accordBody wthree_services_grid_left">
+							<div style='height:10px'></div>
+							<!--h3 class="text-left nomargin" style="font-size:1.5em">1.JEE Mains <span>Repeater</span></h3>
+							<div style='height:10px'></div-->
+							<p>Online computer based testing is the need of an hour. Majority of the entrance exams like JEE Mains, Advanced and MHCET are held in online mode only. TSPH Digital Guru provides a perfect platform to rigorously practice and train for upcoming entrance exams.</p>
+							<div style='height:5px'></div>
+						</div>
+					</div>
+				</div>
+				
+				<!-- MY MISTAKE BOOK -->
+				<div class="card">
+					<div class="card-header" id="headingOne">
+						<h2 class="mb-0 nofocus">
+							<button class="nofocus btn btn-link fullExpand text-left collapsed" type="button" data-toggle="collapse" data-target="#engcollapseFour">
+								<h4 class='accordHeader'>
+									<div class='row nomargin'>
+										<div class='col-xs-11 nopadding' style='overflow: hidden;text-overflow: ellipsis;'><i class="fa fa-play"></i> MY MISTAKE BOOK </div>
+										<div class='col-xs-1 nopadding'>
+											<div style="float: right;">
+												<i class="fa fa-arrow-right"></i>
+												<i class="fa fa-arrow-down"></i>
+											</div>
+										</div>
+									</div>
+								</h4>
+							</button>
+						</h2>
+					</div>
+
+					<div id="engcollapseFour" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+						<div class="card-body accordBody wthree_services_grid_left">
+							<div style='height:10px'></div>
+							<!--h3 class="text-left nomargin" style="font-size:1.5em">1.JEE Advanced 2 Years Course  <span>(XI + XII + JEE Mains &amp; Advanced + MHCET)</span></h3>
+							<div style='height:10px'></div-->
+							<p>It is a customized digital book, keeping track of student’s mistakes. It will help students to identify their weak topics. It will be a perfect book for a student to solve and practice a few months before the actual entrance exam.</p>
+							<!--div style='height:30px'></div-->
+							<div style='height:5px'></div>
+						</div>
+					</div>
+				</div>
+			  
+				<!-- REAL TIME ANALYTICS -->
+				<div class="card">
+					<div class="card-header" id="headingOne">
+						<h2 class="mb-0 nofocus">
+							<button class="nofocus btn btn-link fullExpand text-left collapsed" type="button" data-toggle="collapse" data-target="#engcollapseFive">
+								<h4 class='accordHeader'>
+									<div class='row nomargin'>
+										<div class='col-xs-11 nopadding' style='overflow: hidden;text-overflow: ellipsis;'><i class="fa fa-play"></i> REAL TIME ANALYTICS</div>
+										<div class='col-xs-1 nopadding'>
+											<div style="float: right;">
+												<i class="fa fa-arrow-right"></i>
+												<i class="fa fa-arrow-down"></i>
+											</div>
+										</div>
+									</div>
+								</h4>
+							</button>
+						</h2>
+					</div>
+
+					<div id="engcollapseFive" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+						<div class="card-body accordBody wthree_services_grid_left">
+							<div style='height:10px'></div>
+							<!--h3 class="text-left nomargin" style="font-size:1.5em">1.JEE Mains 1Year Course <span>(XII + JEE Mains + MHCET)</span></h3>
+							<div style='height:10px'></div-->
+							<p>Dashboard of TSPH Digital Guru App provides a comprehensive report of attendance & performance analysis of a student.</p>
+							<div style='height:5px'></div>
+						</div>
+					</div>
+				</div>
+			  
+				<!-- DIGITAL LIBRARY -->
+				<div class="card">
+					<div class="card-header" id="headingOne">
+						<h2 class="mb-0 nofocus">
+							<button class="nofocus btn btn-link fullExpand text-left collapsed" type="button" data-toggle="collapse" data-target="#engcollapseSix">
+								<h4 class='accordHeader'>
+									<div class='row nomargin'>
+										<div class='col-xs-11 nopadding' style='overflow: hidden;text-overflow: ellipsis;'><i class="fa fa-play"></i> DIGITAL LIBRARY </div>
+										<div class='col-xs-1 nopadding'>
+											<div style="float: right;">
+												<i class="fa fa-arrow-right"></i>
+												<i class="fa fa-arrow-down"></i>
+											</div>
+										</div>
+									</div>
+								</h4>
+							</button>
+						</h2>
+					</div>
+
+					<div id="engcollapseSix" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+						<div class="card-body accordBody wthree_services_grid_left">
+							<div style='height:10px'></div>
+							<!--h3 class="text-left nomargin" style="font-size:1.5em">1.JEE Mains <span>Repeater</span></h3>
+							<div style='height:10px'></div-->
+							<p>Apart from printed booklets, study material will also be shared with students in digital format. NCERT and state government textbooks will also be available for reference and solving. Daily Challenge rounds, tutorial sheets and assignments will give rigorous practice.</p>
+							<div style='height:5px'></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			
+			<div class="col-md-12 col-sm-12">
+				<div style="height:80px;"></div>
+			</div>
+			
+			<div class="clearfix"> </div>
+			<div class='text-center'>
+				<div class='btn btn-sm btn-warning' onclick='$("#enquiryForm").modal("show")' style='font-size:1.2em; background-color:#eb5424; border:#eb5424;'>Enquire Now</div>
+			</div>	
+		</div>
+	</div>
+<!-- //services -->
+
+
 
 
 <?php include "include/footer.php"; ?>
